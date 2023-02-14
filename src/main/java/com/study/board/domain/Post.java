5138708a0e088f7171ed034484f6a7ca9cf0a088
ffolabear <ffolabear@gmail.com>
2023-02-14 18:54:26 +0900

    feat(Post): getter, setter 추가

--- src/main/java/com/study/board/domain/Post.java
+++ src/main/java/com/study/board/domain/Post.java
@@ -7,11 +7,41 @@ public class Post {
     String title;
     String content;
 
-    public Post(Long id, String author, String title, String content) {
-        this.id = id;
+    public Post(String author, String title, String content) {
         this.author = author;
         this.title = title;
         this.content = content;
     }
-    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
